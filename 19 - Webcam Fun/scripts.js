aa914f589ffa6f9d43e30d4c361606d42ff78d11
Chris Otto <chris.otto6@gmail.com>
2020-07-07 07:25:06 -0500

    Adding day19 progress

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,35 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector('.player')
+const canvas = document.querySelector('.photo')
+const ctx = canvas.getContext('2d')
+const strip = document.querySelector('.strip')
+const snap = document.querySelector('.snap')
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream)
+      video.src = window.URL.createObjectURL(localMediaStream)
+      video.play()
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth
+  const height = video.videoHeight
+  canvas.width = width
+  canvas.height = height
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 16)
+}
+
+function takePhoto() {
+  snap.currentTime = 0
+  snap.play()
+}
+
+getVideo()
+
+video.addEventListener('canplay', paintToCanvas)
